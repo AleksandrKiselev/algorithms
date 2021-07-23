44a1aced4411424c829c91be7d34a26d0968805f
Aleksandr Kiselev <San4oKiselev@gmail.com>
2021-07-23 21:27:56 +0500

    Dijkstra: refactoring

--- algorithms/shortest_path.h
+++ algorithms/shortest_path.h
@@ -24,12 +24,23 @@ Path find_shortest_path(const AdjacencyMatrix& matrix, Vertex from, Vertex to)
 
 TEST(FindShortestPath, EmptyMatrix)
 {
-	ASSERT_EQ(find_shortest_path(AdjacencyMatrix{}, 0, 0), Path{});
+	const AdjacencyMatrix empty_matrix;
+	const Path empty_path;
+	const Path shortest_path = find_shortest_path(empty_matrix, 0, 0);
+	ASSERT_EQ(shortest_path, empty_path);
 }
 
 TEST(FindShortestPath, OnePointMatrix)
 {
-	ASSERT_EQ(find_shortest_path(AdjacencyMatrix{{ 0 }}, 0, 0), Path{ 0 });
+	const AdjacencyMatrix one_point_matrix{ { 0 } };
+	const Path one_point_path{ 0 };
+	const Path shortest_path = find_shortest_path(one_point_matrix, 0, 0);
+	ASSERT_EQ(shortest_path, one_point_path);
+}
+
+TEST(FindShortestPath, SimpleMatrix)
+{
+
 }
 
 }
